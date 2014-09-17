80e0022649fc1865a67e222b7fab34ed8e50fcd1
David Adair <adair.david@gmail.com>
2014-09-16 18:00:02 -0600

    Events have colorized borders based on type.

--- client/views/shifts/calendar.js
+++ client/views/shifts/calendar.js
@@ -21,12 +21,18 @@ Template.calendar.rendered = function() {
         }
         var startingDate = moment(shift.when.day + ' ' + shift.when.start, "YYYY-MM-DD h:mmA").toDate();
         var endingDate = moment(shift.when.day + ' ' + shift.when.start, "YYYY-MM-DD h:mmA").add(shift.duration, 'hours').toDate();
+        var color = "";
+        if (shift.type == "admin") color = '#556270';
+        else if (shift.type == "student") color = '#4ECDC4';
+        else if (shift.type == "command") color = '#C44D58';
+        else if (shift.type == "nurse") color = '#C7F464';
         var evt = {
           id: shift._id,
           title: title + ' (' + shift.type + ')',
           start: startingDate,
           end: endingDate,
           allDay: false,
+          color: color,
         };
         events.push(evt);
       });
