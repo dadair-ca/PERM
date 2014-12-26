a71b7cdb5132a328f4b3625c149870b284c4089f
David Adair <adair.david@gmail.com>
2014-12-25 22:36:08 -0700

    Emacs likes tabs...

--- server/users.js
+++ server/users.js
@@ -1,67 +1,67 @@
 Accounts.validateNewUser(function (user) {
-  var loggedInUser = Meteor.user();
-
-  if (loggedInUser && Roles.userIsInRole(loggedInUser, 'admin')) {
-    return true;
-  }
-
-  throw new Meteor.Error(403, "Not authorized to create new users.");
-  return false;
+    var loggedInUser = Meteor.user();
+    
+    if (loggedInUser && Roles.userIsInRole(loggedInUser, 'admin')) {
+        return true;
+    }
+    
+    throw new Meteor.Error(403, "Not authorized to create new users.");
+    return false;
 });
 
 Meteor.methods({
-  newUser: function(user) {
-    console.log(user);
-    var id = Accounts.createUser({
-      email: user.email,
-      password: Meteor.uuid().split('-')[0],
-      profile: {
-	  name: user.name,
-	  started: moment(user.started).format(),
-      },
-    });
-
-    if (user.roles.length > 0) {
-      Roles.addUsersToRoles(id, user.roles);
+    newUser: function(user) {
+        console.log(user);
+        var id = Accounts.createUser({
+            email: user.email,
+            password: Meteor.uuid().split('-')[0],
+            profile: {
+	              name: user.name,
+	              started: moment(user.started).format()
+            }
+        });
+        
+        if (user.roles.length > 0) {
+            Roles.addUsersToRoles(id, user.roles);
+        }
+        
+        Accounts.sendEnrollmentEmail(id);
+    },
+    deleteUser: function(user) {
+        var loggedInUser = Meteor.user();
+        
+        if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
+            throw new Meteor.Error(403, "Access denied.");
+        }
+        
+        if (Roles.userIsInRole(user, 'admin')) {
+            throw new Meteor.Error(403, "Cannot delete the administrator.");
+        }
+        
+        var now = moment.tz('America/Edmonton').format();
+        var userShifts = Shifts.find({ownerId: user._id, start: {$gte: now}});
+        userShifts.forEach(function(shift) {
+            Meteor.call('dropShift', shift);
+        });
+        
+        Roles.setUserRoles(user, []);
+        Meteor.users.remove({_id: user._id});
+    },
+    editUser: function(args) {
+        Meteor.users.update(args._id, {$set: args.user});
     }
-
-    Accounts.sendEnrollmentEmail(id);
-  },
-  deleteUser: function(user) {
-    var loggedInUser = Meteor.user();
-
-    if (!loggedInUser || !Roles.userIsInRole(loggedInUser, 'admin')) {
-      throw new Meteor.Error(403, "Access denied.");
-    }
-
-    if (Roles.userIsInRole(user, 'admin')) {
-      throw new Meteor.Error(403, "Cannot delete the administrator.");
-    }
-
-    var now = moment.tz('America/Edmonton').format();
-    var userShifts = Shifts.find({ownerId: user._id, start: {$gte: now}});
-    userShifts.forEach(function(shift) {
-      Meteor.call('dropShift', shift);
-    });
-
-    Roles.setUserRoles(user, []);
-    Meteor.users.remove({_id: user._id});
-  },
-  editUser: function(args) {
-    Meteor.users.update(args._id, {$set: args.user});
-  },
 });
 
 (function () {
-  "use strict";
-
-  Accounts.urls.resetPassword = function(token) {
-    return Meteor.absoluteUrl('reset-password/' + token);
-  };
-  Accounts.urls.verifyEmail = function(token) {
-    return Meteor.absoluteUrl('verify-email/' + token);
-  };
-  Accounts.urls.enrollAccount = function(token) {
-    return Meteor.absoluteUrl('enroll-account/' + token);
-  };
+    "use strict";
+    
+    Accounts.urls.resetPassword = function(token) {
+        return Meteor.absoluteUrl('reset-password/' + token);
+    };
+    Accounts.urls.verifyEmail = function(token) {
+        return Meteor.absoluteUrl('verify-email/' + token);
+    };
+    Accounts.urls.enrollAccount = function(token) {
+        return Meteor.absoluteUrl('enroll-account/' + token);
+    };
 })();
