c7e3d1f078cc9e42ed4448032584019a65f1afcb
David Adair <adair.david@gmail.com>
2015-01-06 13:29:26 -0700

    FIX: Editing username maintains shift stats.

--- client/views/users/edit.js
+++ client/views/users/edit.js
@@ -1,41 +1,45 @@
 Template.usersEdit.events({
-  'click #resetPassword': function(evt) {
-    evt.preventDefault();
-    var options = {email: this.emails[0].address};
+    'click #resetPassword': function(evt) {
+        evt.preventDefault();
+        var options = {email: this.emails[0].address};
 
-    Accounts.forgotPassword(options, function(err) {
-      if (err) {
-        throwFlash('danger', err.reason);
-      } else {
-        throwFlash('success', 'An email has been sent to the specified address which will allow you to reset your password.');
-      }
-    });
-  },
-  'submit form': function(evt) {
-    evt.preventDefault();
+        Accounts.forgotPassword(options, function(err) {
+            if (err) {
+                throwFlash('danger', err.reason);
+            } else {
+                throwFlash('success', 'An email has been sent to the specified address which will allow you to reset your password.');
+            }
+        });
+    },
+    'submit form': function(evt) {
+        evt.preventDefault();
 
-    var currentUserId = this._id;
+        var currentUserId = this._id;
+        var user = this;
 
-    var args = {
-      _id: currentUserId,
-      user: {
-        profile: {
-          name: $('#edit-name').val(),
+        var args = {
+            _id: currentUserId,
+            user: {
+                profile: {
+                    name: $('#edit-name').val(),
+                    started: user.profile.started,
+                    drops: user.profile.drops,
+                    grabs: user.profile.grabs
+                }
+            }
+        };
+
+        if (args.user.profile.name == "") {
+            throwFlash('danger', 'Your name cannot be blank!');
+            return;
         }
-      }
-    };
 
-    if (args.user.profile.name == "") {
-      throwFlash('danger', 'Your name cannot be blank!');
-      return;
-    }
-
-    Meteor.call('editUser', args, function (err) {
-      if (err) {
-        throwFlash('danger', err.reason);
-        return;
-      }
-      throwFlash('success', 'Edited account details.');
-    });
-  },
+        Meteor.call('editUser', args, function (err) {
+            if (err) {
+                throwFlash('danger', err.reason);
+                return;
+            }
+            throwFlash('success', 'Edited account details.');
+        });
+    },
 });
