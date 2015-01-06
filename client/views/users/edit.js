c6fb6b5df1b8a5846deeee29bc1d8029f6e70ed2
David Adair <adair.david@gmail.com>
2015-01-06 14:08:03 -0700

    Added Kadira instrumentation for performance tracking.

--- client/views/users/edit.js
+++ client/views/users/edit.js
@@ -41,5 +41,5 @@ Template.usersEdit.events({
             }
             throwFlash('success', 'Edited account details.');
         });
-    },
+    }
 });
--- server/fixtures.js
+++ server/fixtures.js
@@ -12,6 +12,8 @@
     };
 })();
 
+Kadira.connect('pny4Y9xsh8fznbtt7', 'b17333f7-2c9e-4c62-87a3-b6a19efcbd7d');
+
 if (Meteor.users.find().count() === 0) {
     var roles = ['admin', 'student', 'command', 'nurse'];
     _.each(roles, function(r) { Roles.createRole(r); });
